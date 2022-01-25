5222e974a0a13fb7d058f64bba3fcc54b513e984
rudsa <rudsa@naver.com>
2022-01-25 20:28:01 +0900

    Initial commit

---
+++ src/TicTacToeMain.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TicTacToeMain {
+    Scanner scan = new Scanner(System.in);
+    int enterTicTacToeGameCnt = scan.nextInt();
+
+    public int getEnterTicTacToeGameCnt() {
+        return enterTicTacToeGameCnt;
+    }
+}
